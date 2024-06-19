a580c53277f63fb8fa8f07e30111ed906f698650
Jean Moraes <jean.moraes@t10.digital>
2024-06-19 11:42:37 +0100

    feat: criando cards do dashboard

---
+++ src/components/HeroCard/Index.tsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import VisaoGeral from '../../assets/visao-geral.png';
+
+export const HeroCard = () => {
+    return (
+        <Card sx={{ background: '#FFF3E0' }}>
+			<CardContent>
+				<Box
+					display={'flex'}
+					flexDirection={'row'}
+					alignItems={'center'}
+					justifyContent={'space-evenly'}
+				>
+					<Box>
+						<Typography variant='h5'>Olá, usuário!</Typography>
+						<Typography>Confira as informações da sua casa inteligente!</Typography>
+					</Box>
+					<img src={VisaoGeral} alt='Visão Geral' height={180} />
+				</Box>
+			</CardContent>
+		</Card>
+    )
+}
